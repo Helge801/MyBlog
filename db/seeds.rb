0c1a25531b1bc1258fed868781daa2ec01464355
Bryan Lund <bryan.helge@gmail.com>
2017-12-10 18:40:28 -0700

    adjusted signup input and added users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 15.times do |i|
-  
-# end
+15.times do |i|
+  mypass = RandomWord.adjs(not_shorter_than: 6, not_longer_than: 15).next
+  myname = "#{RandomWord.nouns(not_shorter_than: 3, not_longer_than: 7).next.capitalize} #{RandomWord.adjs(not_shorter_than: 4, not_longer_than: 10).next.capitalize}"
+  User.create!(email: "#{myname.gsub(/\s+/, "")}@mailer.com",
+              password: mypass,
+              password_confirmation: mypass,
+              name: myname,
+              username: myname.split.first + "123"
+              )  
+end
